90c016ce34855c07d32889d6f52fce7bbd31f735
dan_draws <32102806+dandraws21@users.noreply.github.com>
2018-03-25 13:49:37 +0200

    REPLACED project_v2

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerControlls thePlayer;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerControlls>();
+        lastPlayerPosition = thePlayer.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = thePlayer.transform.position;
+            		
+	}
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    public ObjectPooler theObjectPool;
+
+
+    // Use this for initialization
+    void Start () {
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            //Instantiate (thePlatform, transform.position, transform.rotation);
+            GameObject newPlatform = theObjectPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true); 
+
+        }
+		
+	}
+}
